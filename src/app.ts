3f715ad03039764487331719486842849359a014
ThembaMthimunye <218634567@tut4life.ac.za>
2025-04-17 11:14:02 +0200

    yes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 // console.log('Hello, world!');
 import  express from "express";
+import config from "config";
+import connect from "./connect/connect";
+
+const port = config.get<number>("port");
 
 const app=express()
 
-app.listen(1337,()=>{
+ app.listen(port,()=>{
     console.log("Server is running on port 8000");
+     connect()
 })
+
+// KkuUscUACROCUhsu
+// mongodb+srv://218634567:KkuUscUACROCUhsu@cluster0.fuvfg65.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
--- src/config/default.ts
+++ src/config/default.ts
@@ -1,3 +1,4 @@
 export default{
-    port:1337
+    port:1337,
+    dbUri:"mongodb+srv://218634567:KkuUscUACROCUhsu@cluster0.fuvfg65.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 }
